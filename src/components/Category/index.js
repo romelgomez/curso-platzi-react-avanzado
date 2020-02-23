faefe77f780b8d6be98d68e73347dba880514748
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2020-02-23 17:19:11 -0500

    add category component

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Anchor, Image } from './styles'
 
 const DEFAULT_IMG = 'https://i.imgur.com/dJa0Hpl.jpg'
 
 export const Category = ({ cover = DEFAULT_IMG, path = '#', emoji = '?' }) => (
-  <a href={path}>
-    <img src={cover} />
+  <Anchor className={Anchor} href={path}>
+    <Image className={Image} src={cover} />
     {emoji}
-  </a>
+  </Anchor>
 )
