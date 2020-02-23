0a87221b1cc06430bdfee47de1366abcb1514404
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2020-02-23 14:46:17 -0500

    add preset of babel and react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('My project with advance react of platzi!!!');
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h2>Seguimos con el curso avansado</h2>, document.getElementById('app'));
