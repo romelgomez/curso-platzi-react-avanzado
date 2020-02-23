e6a0925a1009ac9289d77608f099eda56ba6b56d
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2020-02-23 14:59:32 -0500

    add linter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h2>Seguimos con el curso avansado</h2>, document.getElementById('app'));
+ReactDOM.render(<h2>Seguimos con el curso avansado</h2>, document.getElementById('app'))
