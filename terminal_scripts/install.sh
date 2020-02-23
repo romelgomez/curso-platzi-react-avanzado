ef4a3241891cf82531807f0dfe880feab4d2a5da
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2020-02-23 12:55:19 -0500

    better install

--- terminal_scripts/install.sh
+++ terminal_scripts/install.sh
@@ -4,4 +4,22 @@ yarn install
 # Api 
 (cd api && yarn install)
 
+# Add commnads
+echo "Available commands:"
+ls $PWD/node_modules/.bin
+export PATH=$PATH:$PWD/node_modules/.bin
 
+
+if [ -z "${COURSE_PLATZI_REACT_AVANCED+xxx}" ]; then 
+    ROOT_PATH='export PATH=$PATH'
+    echo "
+        # curso-platzi-react-avanzado
+        COURSE_PLATZI_REACT_AVANCED=true
+        $ROOT_PATH:$PWD/node_modules/.bin
+        #.................................[/curso-platzi-react-avanzado]
+        " >> ~/.profile && source ~/.profile
+fi
+
+echo 'source ~/.profile' >> ~/.zshrc
+
+echo 'source ~/.profile' >> ~/.bashrc
