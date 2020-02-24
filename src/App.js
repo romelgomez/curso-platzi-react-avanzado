dcd70d2678ebff151ee29f5aad13471a6c058b2d
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2020-02-23 21:15:37 -0500

    add logo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { ListOfCaterories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyle'
 // import { PhotoCard } from './components/PhotoCard'
 import { ListOfPhotoCard } from './components/ListOfPhotoCard'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <div>
+    <Logo />
     <GlobalStyle />
     <div>
       <ListOfCaterories />
---
+++ src/components/Logo/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Svg = styled.svg`
+  width: 200px;
+  margin-top: -30px;
+  margin-left: -10px;
+
+`
