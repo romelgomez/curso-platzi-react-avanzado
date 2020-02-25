7f4e66c2fdbd1200913d3dfaa0c0cda87cccf63e
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2020-02-25 07:10:13 -0500

    add hooks useEffect useState

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import {
   List,
   Item
 } from './styles'
-import { categories } from '../../../api/db.json'
+import { categories as categoriesMock } from '../../../api/db.json'
 
 export const ListOfCaterories = () => {
+
+  const [categories, setCategories] = useState(categoriesMock)
+
+  useEffect(() => {
+    window.fetch('https://petgram-server-romelgomez.now.sh/categories')
+      .then(res => res.json())
+      .then(res => {
+        setCategories(res)
+      })
+  }, [])
+
   return (
     <List>
       {categories.map(category => <Item key={category.id}><Category {...category} /></Item>)}
